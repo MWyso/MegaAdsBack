cdb8e49fb7492f22c30d99f9587ea3a6589f134d
MWyso <mateusz.wyso94@gmail.com>
2023-02-07 10:52:19 +0100

    Added AdRecord.

---
+++ records/ad.record.ts
@@ -0,0 +1,42 @@
+import {AdEntity, NewAdEntity} from "../types";
+import {ValidationError} from "../utils/errors";
+
+export class AdRecord implements AdEntity {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    url: string;
+    lat: number;
+    lon: number;
+
+    constructor(obj: NewAdEntity) {
+        if(!obj.name || obj.name.length > 100) {
+            throw new ValidationError('Nazwa ogłoszenia nie może być pusta, ani przekraczać 100 znaków.');
+        }
+
+        if(obj.description.length > 1000) {
+            throw new ValidationError('Treść ogłoszenia nie może przekraczać 1000 znaków.');
+        }
+
+        if(obj.price < 0 || obj.price > 9999999) {
+            throw new ValidationError('Cena nie może być mniejsza od 0, lub większa niż 9 999 999.');
+        }
+
+        // @TODO Check if url is valid!
+        if(!obj.url || obj.url.length > 100) {
+            throw new ValidationError('Link ogłoszenia nie może być pusty, ani przekraczać 100 znaków');
+        }
+
+        if(typeof obj.lat !== 'number' || typeof obj.lon !== 'number') {
+            throw new ValidationError('Nie można zlokalizować ogłoszenia!');
+        }
+
+        this.name = obj.name;
+        this.description = obj.description;
+        this.price = obj.price;
+        this.url = obj.url;
+        this.lat = obj.lat;
+        this.lon = obj.lon;
+    }
+}
