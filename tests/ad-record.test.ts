625f113d24833ecde07fd4e9a953358f54ec7329
MWyso <mateusz.wyso94@gmail.com>
2023-02-07 13:10:16 +0100

    Added price, name, description test.

--- tests/ad-record.test.ts
+++ tests/ad-record.test.ts
@@ -6,7 +6,7 @@ const defaultObj = {
     url: 'https://megak.pl',
     price: 10,
     lat: 9,
-    lon: 0,
+    lon: 9,
 };
 
 test('Can build AdRecord', () => {
@@ -20,7 +20,50 @@ test('Validates invalid price', () => {
     expect(() => new AdRecord({
         ...defaultObj,
         price: -3,
-    })).toThrow('Cena nie może być mniejsza od 0, lub większa niż 9 999 999.')
-})
+    })).toThrow('Cena nie może być mniejsza od 0, lub większa niż 9 999 999.');
+});
 
-// @TODO: Check all the validations
+test('Validates invalid name', () => {
+    expect(() => new AdRecord({
+        ...defaultObj,
+        name: ''
+    })).toThrow('Nazwa ogłoszenia nie może być pusta, ani przekraczać 100 znaków.');
+
+    expect(() => new AdRecord({
+        ...defaultObj,
+        name: 'a'.repeat(101)
+    })).toThrow('Nazwa ogłoszenia nie może być pusta, ani przekraczać 100 znaków.');
+});
+
+test('Validates invalid description', () => {
+    expect(() => new AdRecord({
+        ...defaultObj,
+        description: 'a'.repeat(1001)
+    })).toThrow('Treść ogłoszenia nie może przekraczać 1000 znaków.');
+});
+// @TODO url valid test
+// test('Validates invalid url', () => {
+//     expect(() => new AdRecord({
+//         ...defaultObj,
+//         url: ''
+//     })).toThrow('Link ogłoszenia nie może być pusty ani przekraczać 100 znaków.');
+//
+//     expect(() => new AdRecord({
+//         ...defaultObj,
+//         url: 'a'.repeat(101)
+//     })).toThrow('Link ogłoszenia nie może być pusty ani przekraczać 100 znaków.');
+// });
+// @TODO lat/lon valid test
+// test('Validates invalid lat/lon', () => {
+//     expect(() => new AdRecord({
+//         ...defaultObj,
+//         lat: 'a' as never,
+//         lon: 9
+//     })).toThrow('Nie można zlokalizować ogłoszenia.');
+//
+//     expect(() => new AdRecord({
+//         ...defaultObj,
+//         lat: 9,
+//         lon: 'a' as never
+//     })).toThrow('Nie można zlokalizować ogłoszenia.');
+// });
