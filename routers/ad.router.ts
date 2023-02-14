5d01d2063206724c75164c698bcf256bfe7b81cc
MWyso <mateusz.wyso94@gmail.com>
2023-02-14 11:01:17 +0100

    grammatical corrections in names

--- routers/ad.router.ts
+++ routers/ad.router.ts
@@ -4,13 +4,13 @@ import {AdRecord} from "../records/ad.record";
 export const adRouter = Router()
 
     .get('/search/:name?', async (req, res) => {
-        const ad = await AdRecord.findAll(req.params.name ?? '');
-        res.json(ad);
+        const ads = await AdRecord.findAll(req.params.name ?? '');
+        res.json(ads);
     })
 
     .get('/:id', async (req, res) => {
-        const ads = await AdRecord.getOne(req.params.id);
-        res.json(ads);
+        const ad = await AdRecord.getOne(req.params.id);
+        res.json(ad);
     })
 
     .post('/', async (req, res) => {
