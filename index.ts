0b2aa3e178af6fd6cd906891554c2a4b7ae209f0
MWyso <mateusz.wyso94@gmail.com>
2023-02-14 13:13:13 +0100

    Added prefix to all URLs.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express, { json, Router } from "express";
 import cors from 'cors';
 import 'express-async-errors';
 import {handleError} from "./utils/errors";
